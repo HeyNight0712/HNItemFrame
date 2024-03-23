cbe254d618f8daf88662481cefdceccee32bb02e
blacknight0712 <blacknight0981@gmail.com>
2024-03-24 05:32:18 +0800

    隱藏展示框

--- src/main/java/heyblock0712/hnitemframe/HNItemFrame.java
+++ src/main/java/heyblock0712/hnitemframe/HNItemFrame.java
@@ -1,5 +1,6 @@
 package heyblock0712.hnitemframe;
 
+import heyblock0712.hnitemframe.listeners.HideFrame;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HNItemFrame extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class HNItemFrame extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new HideFrame(), this);
 
     }
 
---
+++ src/main/java/heyblock0712/hnitemframe/listeners/HideFrame.java
@@ -0,0 +1,55 @@
+package heyblock0712.hnitemframe.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+
+import java.util.Random;
+
+public class HideFrame implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEntityEvent event) {
+        Entity entity = event.getRightClicked();
+        Player player = event.getPlayer();
+
+        // 檢查 蹲下 + 實體:物品展示框
+        if((entity.getType() != EntityType.ITEM_FRAME) || !(player.isSneaking())) return;
+
+        // 檢查玩家手中 物品:藥水
+        ItemStack item = player.getInventory().getItemInMainHand();
+        if (item.getType() != Material.POTION) return;
+
+        // 檢查 藥水:隱形
+        PotionMeta potionMeta = (PotionMeta) item.getItemMeta();
+        if (potionMeta == null || potionMeta.getBasePotionType() != PotionType.LONG_INVISIBILITY) return;
+
+
+        // 設置 隱形展示框
+        ItemFrame itemFrame = (ItemFrame) entity;
+        itemFrame.setVisible(false);
+
+        // 移除 手上物品
+        player.getInventory().setItemInMainHand(null);
+
+        // 回饋音效
+        double distance = player.getLocation().distance(itemFrame.getLocation());
+        float volume = Math.max(1.0f - (float)distance / 10.0f, 0.0f);
+        float pitch = new Random().nextFloat() * 2.0f;
+        player.playSound(player.getLocation(), Sound.ENTITY_ITEM_FRAME_ADD_ITEM, volume, pitch);
+
+        // 取消事件
+        event.setCancelled(true);
+    }
+}
